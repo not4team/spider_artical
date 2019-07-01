3ea9eca90a7f9f1c17734b9c2ce7f76a67009f9c
shixq <shixq3@gmail.com>
2019-07-01 10:15:33 +0800

    setup

--- mitmproxy.bat
+++ mitmproxy.bat
@@ -1,1 +1,1 @@
-mitmproxy --mode=upstream:http://http-dyn.abuyun.com:9020 --upstream-auth=H01234567890123D:0123456789012345
+mitmproxy -s src/mitmdefine.py --mode=upstream:http://http-dyn.abuyun.com:9020 --upstream-auth=H01234567890123D:0123456789012345
---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='ArticalSpider',
+    version='1.0.0',
+    packages=['src'],
+    install_requires=[
+        'configparser>=3.7.4',
+        'selenium>=3.141.0'
+    ]
+)
